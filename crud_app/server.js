cf3482dc405b626ab5978c2f928f650b1349c083
JYOSHREDDY <jyosh2000@gmail.com>
2022-04-09 16:05:53 +0530

    first commit

--- crud_app/server.js
+++ crud_app/server.js
@@ -20,8 +20,6 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
 app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-app.get('/',(req, res)=>{
-    res.render('index');
-})
+app.use('/', require('./server/routes/router'))
 
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
